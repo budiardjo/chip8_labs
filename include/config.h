077c002585eb233bbaaaf727c2305ff12bd4ab56
budiardjo <abrorabyyu9@gmail.com>
2022-12-23 21:33:08 +0700

    add render

--- include/config.h
+++ include/config.h
@@ -1,8 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define EMULATOR_WINDOW_TITLE "Chip8 Emulator"
 #define CHIP8_MEMORY_SIZE 4096
 #define CHIP8_WIDTH 64
 #define CHIP8_HEIGHT 32
+
+// don't know how this works
+#define CHIP8_WINDOW_MULTIPLIER 10
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,20 +1,47 @@
-#include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include "SDL2/SDL.h"
+#include "chip8.h"
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* window = SDL_CreateWindow(
-        "Chip8 Window",
+        EMULATOR_WINDOW_TITLE,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        640, 320, SDL_WINDOW_SHOWN
+        CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER,
+        CHIP8_HEIGHT * CHIP8_WINDOW_MULTIPLIER,
+        SDL_WINDOW_SHOWN
     );
 
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_TEXTUREACCESS_TARGET
+    );
+    while(1)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                goto out;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer,255,255,255,0);
+        SDL_Rect r;
+        r.x = 0;
+        r.y = 0;
+        r.w = 40;
+        r.h = 40;
+        SDL_RenderFillRect(renderer, &r);
+        SDL_RenderPresent(renderer);
+    }
 
-    printf("helo\n");
-    
+out:
     SDL_DestroyWindow(window);
     return 0;
 }
