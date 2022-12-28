4b73ebbcaa009be2bfa6dfe88c8d45b75f5c81d6
budiardjo <abrorabyyu9@gmail.com>
2022-12-28 19:12:01 +0700

    add config

--- src/chip8memory.c
+++ src/chip8memory.c
@@ -1,10 +1,10 @@
 #include "chip8memory.h"
 
-void chip8_memory_set(struct chip8_memory* memory, int index, unsigned char val);
+void chip8_memory_set(struct chip8_memory* memory, int index, unsigned char val)
 {
     memory->memory[index] = val;
 }
-unsigned char chip8_memory_get(struct chip8_memory* memory, int index);
+unsigned char chip8_memory_get(struct chip8_memory* memory, int index)
 {
     return memory->memory[index];
 }
