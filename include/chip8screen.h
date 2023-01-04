53ffc74fac0cb59606efc29a8918eac259ab4d64
budiardjo <abrorabyyu9@gmail.com>
2023-01-04 19:56:51 +0700

    add screen

---
+++ include/chip8screen.h
@@ -0,0 +1,16 @@
+#ifndef CHIP8SCRENN_H
+#define CHIP8SCREEN_H
+
+#include <stdbool.h>
+#include "config.h"
+
+struct chip8_screen
+{
+    // my first multiple array
+    bool pixels[CHIP8_HEIGHT][CHIP8_WIDTH];
+};
+
+void chip8_screen_set(struct chip8_screen* screen, int x, int y);
+bool chip8_screen_is_set(struct chip8_screen* screen, int x, int y);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include "chip8.h"
 #include "chip8keyboard.h"
 
-
 // first time using go to definition in vs code
 // and try to understand how this feture works
 // what a cool feature
@@ -24,8 +23,7 @@ const char keyboard_map[CHIP8_TOTAL_KEYS] = {
     SDLK_c, SDLK_d, SDLK_e, SDLK_f
 };
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 
     struct chip8 chip8;
@@ -55,8 +53,10 @@ int main(int argc, char** argv)
 
     chip8_init(&chip8);
 
+    chip8_screen_set(&chip8.screen, 10, 1);
+
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow(
+    SDL_Window *window = SDL_CreateWindow(
         EMULATOR_WINDOW_TITLE,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
@@ -65,47 +65,48 @@ int main(int argc, char** argv)
         SDL_WINDOW_SHOWN
     );
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(
+    SDL_Renderer *renderer = SDL_CreateRenderer(
         window,
         -1,
         SDL_TEXTUREACCESS_TARGET
     );
-    while(1)
+
+    while (1)
     {
         SDL_Event event;
-        while(SDL_PollEvent(&event))
+        while (SDL_PollEvent(&event))
         {
             // first time using switch
             switch (event.type)
             {
 
-                case SDL_QUIT:
-                    goto out;
+            case SDL_QUIT:
+                goto out;
                 break;
 
-                case SDL_KEYDOWN:
+            case SDL_KEYDOWN:
+            {
+                // why this is weird and long?
+                // what is keysysm?
+                char key = event.key.keysym.sym;
+                int vkey = chip8_keyboard_map(keyboard_map, key);
+                if (vkey != -1)
                 {
-                    // why this is weird and long?
-                    // what is keysysm?
-                    char key = event.key.keysym.sym;
-                    int vkey = chip8_keyboard_map(keyboard_map, key);
-                    if (vkey != -1)
-                    {
-                        chip8_keyboard_down(&chip8.keyboard, vkey);
-                    }
+                    chip8_keyboard_down(&chip8.keyboard, vkey);
                 }
-                break;
+            }
+            break;
 
-                case SDL_KEYUP:
+            case SDL_KEYUP:
+            {
+                char key = event.key.keysym.sym;
+                int vkey = chip8_keyboard_map(keyboard_map, key);
+                if (vkey != -1)
                 {
-                    char key = event.key.keysym.sym;
-                    int vkey = chip8_keyboard_map(keyboard_map, key);
-                    if (vkey != -1)
-                    {
-                        chip8_keyboard_up(&chip8.keyboard, vkey);
-                    }
+                    chip8_keyboard_up(&chip8.keyboard, vkey);
                 }
-                break;
+            }
+            break;
             };
 
             if (event.type == SDL_QUIT)
@@ -115,13 +116,25 @@ int main(int argc, char** argv)
         }
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer,255,255,255,0);
-        SDL_Rect r;
-        r.x = 0;
-        r.y = 0;
-        r.w = 40;
-        r.h = 40;
-        SDL_RenderFillRect(renderer, &r);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+        // omg, my first loop
+        // it even using XOR Calculator
+        for (int x = 0; x < CHIP8_WIDTH; x++)
+        {
+            for (int y = 0; y < CHIP8_HEIGHT; y++)
+            {
+                if (chip8_screen_is_set(&chip8.screen, x, y))
+                {
+                    SDL_Rect r;
+                    r.x = x * CHIP8_WINDOW_MULTIPLIER;
+                    r.y = y * CHIP8_WINDOW_MULTIPLIER;
+                    r.w = CHIP8_WINDOW_MULTIPLIER;
+                    r.h = CHIP8_WINDOW_MULTIPLIER;
+                    SDL_RenderFillRect(renderer, &r);
+                }
+            }
+        }
+
         SDL_RenderPresent(renderer);
     }
 
